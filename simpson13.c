2810dfe7f5ba4592663056794dce4930bc875929
roshanlimbu <birenlimbu415@gmail.com>
2024-02-06 09:52:35 +0545

    simson 1/3 rule

--- simpson13.c
+++ simpson13.c
@@ -1,24 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-float f( float x)
-{
+#include <stdio.h>
+float f(float x) {
   float z;
-  z=sqrt(1-x*x);
+  z = sqrt(1 - x * x);
   return z;
 }
-int main()
-{
-  float x1,x2,xn,h,v,n;
+int main() {
+  float x1, x2, xn, h, v, n;
   printf("Enter the value of lower limit:\n");
   scanf("%f", &x1);
   printf("Enter the value of upper limit:\n");
   scanf("%f", &xn);
-  n=2;
-  h=(xn-x1)/n;
-  x2=x1+h;
-  v=h/3*(f(x1)+4*f(x2)+f(xn));
+  n = 2;
+  h = (xn - x1) / n;
+  x2 = x1 + h;
+  v = h / 3 * (f(x1) + 4 * f(x2) + f(xn));
   printf("The value of integration is=%f", v);
   return 0;
-  
 }

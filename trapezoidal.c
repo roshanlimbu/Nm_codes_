5cef0a0d183e704fba18cb73ed69974e14bf59de
roshanlimbu <birenlimbu415@gmail.com>
2024-02-06 09:55:22 +0545

    trapezoidal updated

--- trapezoidal.c
+++ trapezoidal.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-float f( float x)
-{
-    float z;
-    z=x*x*x+2;
-    return z;
+float f(float x) {
+  float z;
+  z = x * x * x + 2;
+  return z;
 }
-int main()
-{
-    int x1,x2;
-    float h,v;
-    printf("Enter the value of lower limit\n");
-    scanf("%d", &x1);
-     printf("Enter the value of upper limit\n");
-    scanf("%d", &x2);
-    h=x2-x1;
-    v=h/2*(f(x1)+f(x2));
-    printf("The value of integration is= %f\n", v);
-    return 0;
+int main() {
+  int x1, x2;
+  float h, v;
+  printf("Enter the value of lower limit\n");
+  scanf("%d", &x1);
+  printf("Enter the value of upper limit\n");
+  scanf("%d", &x2);
+  h = x2 - x1;
+  v = h / 2 * (f(x1) + f(x2));
+  printf("The value of integration is= %f\n", v);
+  return 0;
 }

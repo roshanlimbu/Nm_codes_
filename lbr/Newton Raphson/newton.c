93f52ebe611a1a5cd8d2137e2d0a6b57b252aa2d
roshanlimbu <birenlimbu415@gmail.com>
2024-04-26 15:30:05 +0545

    -newton raphson

---
+++ lbr/Newton Raphson/newton.c
@@ -0,0 +1,27 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+float f(float x) {
+  float z;
+  z = x * x + 2 * x - 2;
+  return z;
+}
+float df(float x) {
+  float p;
+  p = 2 * x + 2;
+  return p;
+}
+int main() {
+  float x1, x0;
+  float e = 0.0001;
+  int itr = 1;
+  printf("Enter the value of x0:\n");
+  scanf("%f", &x0);
+  while (fabs(f(x0)) > e) {
+    x1 = x0 - f(x0) / df(x0);
+    x0 = x1;
+    printf("Iteration %d root = %f\n", itr, x0);
+    itr++;
+  }
+  return 0;
+}
---
+++ lbr/bisectionM/bisection.c
@@ -0,0 +1,34 @@
+// bisection method for the function
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+float f(float x) {
+  float z;
+  z = x * x * x - 2 * x - 5;
+  return z;
+}
+int main() {
+  float x1, x2, x0;
+  float e = 0.001;
+  printf("Enter the value of x1:\n");
+  scanf("%f", &x1);
+  printf("Enter the value of x2:\n");
+  scanf("%f", &x2);
+  printf("Initial guess %f %f\n", x1, x2);
+  if (f(x1) * f(x2) > 0) {
+    printf("wrong initial guess!!!");
+    exit(0);
+  }
+  x0 = (x1 + x2) / 2;
+  while (fabs(f(x0)) > e) {
+    if (f(x0) * f(x1) > 0) {
+      x1 = x0;
+    } else {
+      x2 = x0;
+    }
+    x0 = (x1 + x2) / 2;
+    printf("x1=%f x2=%f x0=%f f(x0)=%f\n", x1, x2, x0, f(x0));
+  }
+  printf("The root = %f ", x0);
+  return 0;
+}
